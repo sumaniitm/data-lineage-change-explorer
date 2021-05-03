dc7e1a374a561fbb2000e735f903d6d8e0757d37
Ubuntu <ubuntu@ip-172-31-47-133.us-west-2.compute.internal>
2021-05-03 11:38:25 +0000

    modifications to allow vertex and edge list input via jsons

--- createGraph.py
+++ createGraph.py
@@ -8,21 +8,31 @@ from graph import Graph
 
 ## This module will aim to create the adjacency matrix based on inputs
 ## inputs are a list of vertices and a list of tuples where the tuples are made of the two vertices which makes an edge and the cost associated with that edge
+## inputs are provided thru two json files
 ## e.g. createGraph(['a','b','c'], [('a','b',10),('a','c',20),('b','c',30)])
 
-## provide json as vertices and edges
-with open('vertices.json','r') as f:
-    vertices_config = json.load(f)
-    
-listOfVertices=[]
-
-for i in range(0,len(vertices_config['vertices'])):
-    listOfVertices.append(vertices_config['vertices'][i]['vertex_id'])
-
 class createGraph:
-    def __init__(self, listOfVertices=[], listOfEdges=[]):
-        self.listOfVertices = listOfVertices
-        self.listOfEdges = listOfEdges
+    def __init__(self, vertexJsonFile='vertices.json', edgeJsonFile='edges.json'):
+        self.vertexJsonFile = vertexJsonFile
+        self.edgeJsonFile = edgeJsonFile
+        
+        ## provide json as vertices and edges
+        with open('vertices.json','r') as f:
+            vertices_config = json.load(f)
+    
+        listOfVertices=[]
+
+        for i in range(0,len(vertices_config['vertices'])):
+            listOfVertices.append(vertices_config['vertices'][i]['vertex_id'])
+        
+        with open('edges.json','r') as f:
+            edges_config = json.load(f)
+            
+        listOfEdges=[]
+        
+        for i in range(0,len(edges_config['edges'])):
+            listOfEdges.append( (edges_config['edges'][i]['from_vertex_id'], edges_config['edges'][i]['to_vertex_id'], edges_config['edges'][i]['edge_id']) )
+
         g = Graph(len(listOfVertices))
         # create the vertices of the graph
         for v in range(0,len(listOfVertices)):
@@ -40,8 +50,7 @@ class createGraph:
                 toDict.append(to)
                 #ensure only directed graphs are allowed. Is count the most optimal way of doing this ??
                 if (fromDict.count(to) > 0 and toDict.count(frm) > 0):
-                    print("only directed graph is allowed")
-                    return
+                    print("only directed graph is allowed, this edge won't be created")
                 else:
                     g.addEdge(frm, to, cost)
             
