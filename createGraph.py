a1a923ea71fd3db41372105431d885208692f010
Ubuntu <ubuntu@ip-172-31-47-133.us-west-2.compute.internal>
2021-05-09 04:14:31 +0000

    removing redundant code and adding graceful termination on wrong inputs

--- createGraph.py
+++ createGraph.py
@@ -16,23 +16,12 @@ from graph import Graph
 ##    j is the final value of the vertex j after all the transformations are applied (which can have contributions from other vertices as well)
 
 class createGraph:
-    def __init__(self, vertexJsonFile='vertices.json', edgeJsonFile='edges.json'):
+    def __init__(self, vertexJsonFile='vertices.json', edgeJsonFile='edges.json', lookupJsonFile='lookupPast.json'):
         self.vertexJsonFile = vertexJsonFile
         self.edgeJsonFile = edgeJsonFile
+        self.lookupJsonFile = lookupJsonFile
         
-        ## provide json as vertices and edges
-        with open('vertices.json','r') as f:
-            vertices_config = json.load(f)
-    
-        listOfVertices=[]
-
-        for i in range(0,len(vertices_config['vertices'])):
-            listOfVertices.append(vertices_config['vertices'][i]['vertex_id'])
-        
-        with open('edges.json','r') as f:
-            edges_config = json.load(f)
-            
-        listOfEdges=[]
+        ## provide json as vertices, edges and past edge lookups
         
         ## The edge value represents the output of applying the transformation rule mentioned in edge_description on the from_vertex_id
         ## The vertex value represents the starting value of the data attribute corresponding to the vertex before any transformation is applied on it
@@ -40,20 +29,61 @@ class createGraph:
         ## the edge values of a-c and b-c will be a^2 and b^2 respectively, while the vertex value of c will be a^2 + b^2 + 2ab
         ## the vertex value of a and b are a and b respectively
         
-        for i in range(0,len(edges_config['edges'])):
-            listOfEdges.append( (edges_config['edges'][i]['from_vertex_id'], edges_config['edges'][i]['to_vertex_id'], (edges_config['edges'][i]['from_vertex_value'], edges_config['edges'][i]['edge_value']) ) )
+        with open('vertices.json','r') as f:
+            vertices_config = json.load(f)
+    
+        self.listOfVertices=[]
 
-        g = Graph(len(listOfVertices))
+        for i in range(0,len(vertices_config['vertices'])):
+            self.listOfVertices.append(vertices_config['vertices'][i]['vertex_id'])
+        
+        with open('edges.json','r') as f:
+            edges_config = json.load(f)
+            
+        self.listOfEdges=[]
+        self.listOfEdgesToCompare=[]
+        
+        for i in range(0,len(edges_config['edges'])):
+            self.listOfEdges.append( (edges_config['edges'][i]['from_vertex_id'], edges_config['edges'][i]['to_vertex_id'], ( edges_config['edges'][i]['from_vertex_value'], edges_config['edges'][i]['edge_value'] ) ) )
+            self.listOfEdgesToCompare.append( (edges_config['edges'][i]['from_vertex_id'], edges_config['edges'][i]['to_vertex_id']) )
+        
+        with open('lookupPast.json','r') as f:
+            lookup_config = json.load(f)
+            
+        self.listOfLookupEdges=[]
+        self.listOfLookupEdgesToCompare=[]
+        
+        for i in range(0,len(lookup_config['edges'])):
+            self.listOfLookupEdges.append( (lookup_config['edges'][i]['from_vertex_id'], lookup_config['edges'][i]['to_vertex_id'], (lookup_config['edges'][i]['from_vertex_value'], lookup_config['edges'][i]['edge_value']) ) )
+            self.listOfLookupEdgesToCompare.append( (lookup_config['edges'][i]['from_vertex_id'], lookup_config['edges'][i]['to_vertex_id']) )
+    
+    def checkForValidLookup(self):
+        ## check if the lookup.json is of same structure as the edges.json . This is a hard requirement
+        result = True
+        if len(self.listOfLookupEdges) != len(self.listOfEdges):
+            print("the total number of edges in edges.json and lookupPast.json are different; cannot create delta, exiting!")
+            result = False
+        else:
+            for e in range(0,len(self.listOfEdges)):
+                if self.listOfEdgesToCompare[e] != self.listOfLookupEdgesToCompare[e]:
+                    print("The {0}th edge between edges.json and lookupPast.json are different; cannot create delta, exiting!".format(e))
+                    result = False
+                    break
+        return result
+        
+    ## This method will represent the current value matrix
+    def createValueMatrix(self):
+        g = Graph(len(self.listOfVertices))
         # create the vertices of the graph
-        for v in range(0,len(listOfVertices)):
-            g.setVertex(v,listOfVertices[v])
+        for v in range(0,len(self.listOfVertices)):
+            g.setVertex(v,self.listOfVertices[v])
         # establish the edges
         fromDict=[]
         toDict=[]
-        for e in range(0,len(listOfEdges)):
-            frm,to,cost = listOfEdges[e]
+        for e in range(0,len(self.listOfEdges)):
+            frm,to,cost = self.listOfEdges[e]
             # keeping a track of the starting and ending vertices of the edges. Is this the most optimal storage ??
-            if (listOfVertices.count(frm) == 0 or listOfVertices.count(to) == 0):
+            if (self.listOfVertices.count(frm) == 0 or self.listOfVertices.count(to) == 0):
                 print("any one of the provided vertex is not in the list of vertices, this edge won't be created")
             else:
                 fromDict.append(frm)
@@ -63,6 +93,34 @@ class createGraph:
                     print("only directed graph is allowed, this edge won't be created")
                 else:
                     g.addEdge(frm, to, cost)
-            
         g.printMatrix()
         
+    def createDeltaMatrix(self):
+        ## check for equivalence of edges.json and lookup.json in terms of edges.
+        if not self.checkForValidLookup():
+            return
+        
+        gDelta = Graph(len(self.listOfVertices))
+        # create the vertices of the graph
+        for v in range(0,len(self.listOfVertices)):
+            gDelta.setVertex(v,self.listOfVertices[v])
+        # establish the edges
+        fromDict=[]
+        toDict=[]
+        
+        for e in range(0,len(self.listOfLookupEdges)):
+            frmLkp,toLkp,costLkp = self.listOfLookupEdges[e]
+            frm,to,costCurr = self.listOfEdges[e]
+            cost = tuple(map(lambda i, j: round((i - j)/j,2), costCurr, costLkp))
+            # keeping a track of the starting and ending vertices of the edges. Is this the most optimal storage ??
+            if (self.listOfVertices.count(frm) == 0 or self.listOfVertices.count(to) == 0):
+                print("any one of the provided vertex is not in the list of vertices, this edge won't be created")
+            else:
+                fromDict.append(frm)
+                toDict.append(to)
+                #ensure only directed graphs are allowed. Is count the most optimal way of doing this ??
+                if (fromDict.count(to) > 0 and toDict.count(frm) > 0):
+                    print("only directed graph is allowed, this edge won't be created")
+                else:
+                    gDelta.addEdge(frm, to, cost)
+        gDelta.printMatrix()
