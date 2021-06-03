43af0b384a0c2d5decae8b8373bb511d4c4c02b8
Ubuntu <ubuntu@ip-172-31-47-133.us-west-2.compute.internal>
2021-06-03 16:33:11 +0000

    stage 2 working version of app, getting there

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 import os
 from display import displayDataLineage
 
@@ -9,11 +9,20 @@ print(__name__)
 ddl = displayDataLineage()
 
 lineage = ddl.showAttributeLineage()
+deltaLineage = ddl.showDeltaLineage()
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', lineage=lineage)
+    return render_template('index.html')
+
+@app.route('/attributeLineage')
+def attributeLineage():
+    return render_template('attributeLineage.html', lineage=lineage)
+    
+@app.route('/attributeDeltaLineage')
+def attributeDeltaLineage():
+    return render_template('attributeDeltaLineage.html', lineage=deltaLineage)
 
 if __name__ == '__main__':
     #app.run(debug=True)
